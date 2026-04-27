f8e5e0df3a4f29523416385d86e696211adaae5f
p-mariia-v <Pushkarova.m.v@gmail.com>
2026-04-27 19:34:03 +0200

    Update my-page.js

--- my-page.js
+++ my-page.js
@@ -17,7 +17,7 @@ function changeYear(yearValue) {
     workbook.changeParameterValueAsync('Year Parameter', yearValue)
         .then(function() {
             console.log("Year changed to " + yearValue);
-        })
+        });
 }  
 function exportPDF() {
     if (viz) {
