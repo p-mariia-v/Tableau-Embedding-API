d41edde98d9b815d868568836446ad06a12b3451
p-mariia-v <Pushkarova.m.v@gmail.com>
2026-04-27 16:21:35 +0200

    Create my-page.js

---
+++ my-page.js
@@ -0,0 +1,22 @@
+  var viz;
+
+  function initViz() {
+    var containerDiv = document.getElementById("vizContainer"),
+    url = "https://public.tableau.com/views/MARKETCOMPARISONOFLEADINGCARBRANDS/Overview";
+    var options = {
+        hideTabs: true,
+        onFirstInteractive: function () {
+            console.log("Run this code when the viz has finished loading.");
+        }
+    };
+    viz = new tableau.Viz(containerDiv, url, options);
+  }
+function changeYear(yearValue) {
+    var workbook = viz.getWorkbook();
+
+    workbook.changeParameterValueAsync('Year Parameter Name', yearValue)
+        .then(function() {
+            console.log("Year changed to " + yearValue);
+        });
+}
+window.onload = initViz;
