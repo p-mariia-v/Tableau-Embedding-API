c5f57729cb1f300c9d66a480eaf859ec1c97aa50
p-mariia-v <Pushkarova.m.v@gmail.com>
2026-04-28 10:22:43 +0200

    Update my-page.js

--- my-page.js
+++ my-page.js
@@ -29,4 +29,35 @@ function exportPDF() {
 function resetViz() {
     viz.revertAllAsync();
 }
+
+let activeBrands = ["BMW", "Mercedes", "Audi", "Porsche", "Volkswagen"];
+
+function toggleBrand(buttonElement, brandName) {
+    // 1. Update our tracking array
+    if (activeBrands.includes(brandName)) {
+        // If it's there, remove it (Exclude)
+        activeBrands = activeBrands.filter(item => item !== brandName);
+        buttonElement.classList.replace('active', 'inactive');
+    } else {
+        // If it's not there, add it back (Include)
+        activeBrands.push(brandName);
+        buttonElement.classList.replace('inactive', 'active');
+    }
+    applyBrandFilter();
+}
+
+function applyBrandFilter() {
+    var workbook = viz.getWorkbook();
+    var activeSheet = workbook.getActiveSheet();
+    var worksheets = activeSheet.getWorksheets();
+
+    for (var i = 0; i < worksheets.length; i++) {
+        // We tell Tableau: "Only show brands that are in my activeBrands list"
+        worksheets[i].applyFilterAsync(
+            "Brand", 
+            activeBrands, 
+            tableau.FilterUpdateType.REPLACE
+        );
+    }
+}
 window.onload = initViz;
